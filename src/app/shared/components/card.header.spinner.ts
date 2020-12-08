482696997cf92558acb4a937c9aab9250e840666
Wim Kielen <wim_kielen@hotmail.com>
2020-12-08 16:17:54 +0100

    string met plussen vervangen door back-tick

--- src/app/shared/components/card.header.spinner.ts
+++ src/app/shared/components/card.header.spinner.ts
@@ -5,13 +5,16 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'mat-card-header-spinner',
-  template:'<mat-card-header>' +
-  '<mat-card-title> {{ title }}</mat-card-title>' +
-  '<div style="flex: 1;"></div>' +
-    '<div fxFlex fxLayout="row" fxLayoutAlign="flex-end">' +
-    '<mat-spinner color="warn" mode="determinate" diameter="35" [value]="progress * 1.5" strokeWidth="7">' +
-    '</mat-spinner>' +
-    '</div></mat-card-header>',
+  template: `
+  <mat-card-header>
+    <mat-card-title> {{ title }}</mat-card-title>
+      <div style="flex: 1;"></div>
+      <div fxFlex fxLayout='row' fxLayoutAlign='flex-end'>
+        <mat-spinner color="warn" mode="determinate" diameter="35" [value]="progress * 1.5" strokeWidth="7">
+        </mat-spinner>
+      </div>
+    </mat-card-header>
+  `,
   styles: ['mat-spinner { margin-top: 3px; margin-right: 20px; }']
 })
 
