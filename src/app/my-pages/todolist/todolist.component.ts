a0a5752abbf27ebe3b78df99b665202be4217807
Wim <wim_kielen@hotmail.com>
2020-12-12 21:49:03 +0100

    stukje verder

--- src/app/my-pages/todolist/todolist.component.ts
+++ src/app/my-pages/todolist/todolist.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { MatTableDataSource } from '@angular/material/table';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { ActionItem, ActionService } from 'src/app/services/action.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { NotificationService } from 'src/app/services/notification.service';
+import { Dictionary } from 'src/app/shared/modules/Dictionary';
 import { ParentComponent } from 'src/app/shared/parent.component';
 import { TodoListDetailDialogComponent } from './todolist.detail.dialog';
 
@@ -15,16 +16,20 @@ import { TodoListDetailDialogComponent } from './todolist.detail.dialog';
 })
 export class TodolistComponent extends ParentComponent implements OnInit {
 
+  // @ViewChild(MatTable, {static: false}) tableOpenActions: MatTable<any>;
+
+
   deleteprogress1 = 0;
   deleteprogress2 = 0;
   deleteprogress3 = 0;
   headerToggleChecked: boolean = false;
-  actionList: Array<ActionItem> = [];
+  actionList: Dictionary = new Dictionary([]);
 
   columnsToDisplay: string[] = ['Title', 'HolderName', 'StartDate', 'TargetDate', 'actions3'];
   filterValues = { LidNr: '' };
   filterFininshedValues = { LidNr: '' };
   dataSourceOpenActions = new MatTableDataSource<ActionItem>();
+
   dataSourceFinishedActions = new MatTableDataSource<ActionItem>();
   selectedAction: ISelectedAction;
 
@@ -39,41 +44,87 @@ export class TodolistComponent extends ParentComponent implements OnInit {
   ngOnInit(): void {
 
     this.actionList = this.actionService.GetSome();
-    this.dataSourceOpenActions.data = this.actionList;
+
+    this.dataSourceOpenActions.data = this.actionList.values();
     this.dataSourceOpenActions.filterPredicate = this.createOpenActionFilter();
     this.filterValues.LidNr = '';
     this.dataSourceOpenActions.filter = JSON.stringify(this.filterValues);
 
-    this.dataSourceFinishedActions.data = this.actionList;
+    this.dataSourceFinishedActions.data = this.actionList.values();
     this.dataSourceFinishedActions.filterPredicate = this.createFinishedActionFilter();
     this.filterFininshedValues.LidNr = '';
     this.dataSourceFinishedActions.filter = JSON.stringify(this.filterFininshedValues);
 
   }
 
-  onAddAction(): void {
-    console.log('Add action',);
+/***************************************************************************************************
+/ De klik op de Done en Delete knop start 'timer' in de header. Na een seconde komt er een event
+/ Dit event kikt deze method af.
+/ Aan de hand van de selected item bepaal ik welke actie is doe op welk record
+/***************************************************************************************************/
+  onHoldAction($event): void {   // uit de header
+    console.log('onHoldAction', this.selectedAction);
+    // Dus Done knop. Vul Eind datum in zodat hij doorschuift naar volgende tabel
+    if (this.selectedAction.operation == "done") {
+      const id = this.selectedAction.action.Id;
+      let action: ActionItem = this.actionList.get(id)
+      action.EndDate = '2020-01-01';
+      this.refreshFilters();
+      return;
+    }
+
+    if (this.selectedAction.operation == "delete") {
+      const id = this.selectedAction.action.Id;
+      this.actionList.remove(id);
+      this.refreshFilters();
+      return;
+    }
+
+
+
   }
-  onDoneAction($event, index: number): void {
+
+  refreshFilters() {
+    this.dataSourceOpenActions.filter = JSON.stringify(this.filterValues);
+    this.dataSourceFinishedActions.filter = JSON.stringify(this.filterValues);
+  }
+
+
+  /***************************************************************************************************
+  / Open Actions Table Add Knop
+  /***************************************************************************************************/
+  onAddOpenAction(): void {
+    let x = new ActionItem();
+    x.Id = "9";
+    x.StartDate = '2020-12-01';
+    x.LidNr = "23";
+    x.TargetDate = '2020-12-02';
+    x.HolderName = 'Wim';
+    x.Title = 'Action9';
+    x.Finished = "0";
+    x.Description = 'Beschrijving 1'
+    console.log('Add action',);
+    this.actionList.add(x.Id, x)
+    this.dataSourceOpenActions.filter = JSON.stringify(this.filterValues);
+    console.log('list ', this.actionList);
+
+  }
+
+  /***************************************************************************************************
+  / Open Actions Table Done Knop
+  / De actie zelf gaat via het event uit de header onHoldAction
+  /***************************************************************************************************/
+  onDoneOpenAction($event, index: number): void {
     this.deleteprogress1 = $event / 10;
     if (this.deleteprogress1 == 0) {
-      this.selectedAction = { operation: "done", action: this.dataSourceOpenActions.data[index] };
+      this.selectedAction = { operation: "done", action: this.dataSourceOpenActions.filteredData[index] };
     }
   }
 
-  onHoldAction($event): void {   // uit de header
-    console.log('onHoldAction', $event);
-    if (this.selectedAction.operation == "done") {
-      console.log('done geselecteerd', this.selectedAction.action);
-      this.dataSourceOpenActions.data[0].EndDate = '2020-01-01'
-      console.log('', this.dataSourceOpenActions.data);
-      this.dataSourceOpenActions.filter = JSON.stringify(this.filterValues);
-      this.dataSourceFinishedActions.filter = JSON.stringify(this.filterValues);
 
-    }
-
-  }
-
+  /***************************************************************************************************
+  / Open Actions Table Edit Knop
+  /***************************************************************************************************/
   onEditAction(index: number): void {
     let toBeEdited: ActionItem = this.dataSourceOpenActions.data[index];
 
@@ -91,9 +142,20 @@ export class TodolistComponent extends ParentComponent implements OnInit {
   }
 
 
+  /***************************************************************************************************
+  / Open Actions Table Delete Knop
+  / De actie zelf gaat via het event uit de header onHoldAction
+  /***************************************************************************************************/
   onDeleteAction($event, index: number): void {
     this.deleteprogress1 = $event / 10;
+    if (this.deleteprogress1 == 0) {
+      this.selectedAction = { operation: "delete", action: this.dataSourceOpenActions.filteredData[index] };
+    }
   }
+
+  /***************************************************************************************************
+  / Open Actions Table Dubbel Klik op regels geeft details
+  /***************************************************************************************************/
   onDblclickAction($event, index: number): void {
     let toBetoBeShown: ActionItem = this.dataSourceOpenActions.data[index];
 
@@ -113,6 +175,8 @@ export class TodolistComponent extends ParentComponent implements OnInit {
     }
     this.dataSourceOpenActions.filter = JSON.stringify(this.filterValues);
   }
+
+
 
   onSliderFinishedChanged($event): void {
     if ($event.checked) {
--- src/app/services/action.service.ts
+++ src/app/services/action.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { Dictionary } from '../shared/modules/Dictionary';
 import { DataService } from './data.service';
 
 @Injectable({
@@ -14,6 +15,10 @@ export class ActionService extends DataService {
   }
 
   GetSome() {
+
+    let actions: Dictionary = new Dictionary([]);
+
+
     let list:Array<ActionItem> = [];
     let x = new ActionItem();
     x.Id = "1";
@@ -24,6 +29,7 @@ export class ActionService extends DataService {
     x.Title = 'Action1';
     x.Finished = "0";
     x.Description = 'Beschrijving 1'
+    actions.add(x.Id, x);
     list.push(x);
     x = new ActionItem();
     x.Id = "2";
@@ -34,6 +40,8 @@ export class ActionService extends DataService {
     x.TargetDate = '2020-12-03';
     x.Description = 'Beschrijving 2\nTweede regel';
     list.push(x);
+    actions.add(x.Id, x);
+
     x = new ActionItem();
     x.Id = "3";
     x.Title = 'Action3';
@@ -43,6 +51,7 @@ export class ActionService extends DataService {
     x.Description = 'Beschrijving 3';
     x.HolderName = 'Kees';
     list.push(x);
+    actions.add(x.Id, x);
 
     x = new ActionItem();
     x.Id = "4";
@@ -55,8 +64,9 @@ export class ActionService extends DataService {
     x.EndDate = '2020-12-01';
 
     list.push(x);
+    actions.add(x.Id, x);
 
-    return list;
+    return actions;
   }
 
 
