6d077a2b7dde0b7b93246a2ab174e98ebcc18c59
Wim Kielen <wim_kielen@hotmail.com>
2021-03-10 19:41:52 +0100

    dubbele service-worker

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,8 +8,18 @@ export const environment = {
   mailUrl: 'http://84.104.233.194:5001',
 
   // de proxy url stuurt requests door naar de Rasp.Pi. Dit kan ook direct natuurlijk maar we gebruiken deze proxy om de https te ontwijken op de Pi
-  // de service worker wil namelijk alleen via https en de pi heeft dit niet. 
-  proxyUrl: 'https://www.ttvn.nl/api/proxy/'
+  // de service worker wil namelijk alleen via https en de pi heeft dit niet.
+  proxyUrl: 'https://www.ttvn.nl/api/proxy/',
 
 
+  firebase: {
+    apiKey: "AIzaSyBrkqBOtSElrG76AIjsaHe9SrmZA_0gjrY",
+    authDomain: "ttvn-app.firebaseapp.com",
+    databaseURL: "https://ttvn-app-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "ttvn-app",
+    storageBucket: "ttvn-app.appspot.com",
+    messagingSenderId: "953979335612",
+    appId: "1:953979335612:web:7cdfc82b92decee36231b9",
+    measurementId: "G-FKPQBQ8CTR"
+  }
 };
--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -2,7 +2,14 @@ importScripts('https://www.gstatic.com/firebasejs/7.6.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.6.0/firebase-messaging.js');
 
 firebase.initializeApp({
- messagingSenderId: "953979335612"
+  apiKey: "AIzaSyBrkqBOtSElrG76AIjsaHe9SrmZA_0gjrY",
+  authDomain: "ttvn-app.firebaseapp.com",
+  databaseURL: "https://ttvn-app-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "ttvn-app",
+  storageBucket: "ttvn-app.appspot.com",
+  messagingSenderId: "953979335612",
+  appId: "1:953979335612:web:7cdfc82b92decee36231b9",
+  measurementId: "G-FKPQBQ8CTR"
 });
 
 const messaging = firebase.messaging();
