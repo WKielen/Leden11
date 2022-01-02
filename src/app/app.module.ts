89309568ab1ee70cab1405ee008fbf7a9dba1429
Wim <wim_kielen@hotmail.com>
2022-01-02 20:56:31 +0100

    service worker working on 8080

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -49,7 +49,7 @@ registerLocaleData(localeNl);
     MyPagesModule,
     // Registratie van sw veranderd omdat we niet in de base href zitten maar in admin.
     // Note: if you are using a base-href in production, you’ll need to change the '/ngsw-worker.js' path to './ngsw-worker.js' to prevent a 404 error.
-    ServiceWorkerModule.register('./main-sw.js', { enabled: environment.production, registrationStrategy: 'registerImmediately' }),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production, registrationStrategy: 'registerImmediately' }),
   ],
 
   providers: [AuthService,
--- src/app/my-pages/aa notes.ts
+++ src/app/my-pages/aa notes.ts
@@ -30,34 +30,8 @@
 //// Dit is a strike through
 /*
 
-ALTER TABLE Lid DROP COLUMN BIC;
-ALTER TABLE Lid DROP COLUMN ToernooiSpeler;
-ALTER TABLE Lid DROP COLUMN Gemerkt;
-ALTER TABLE Lid DROP COLUMN Korting;
-ALTER TABLE Lid DROP COLUMN Geincasseerd;
-ALTER TABLE Lid DROP COLUMN VrijwillgersRegelingIsVanToepassing;
-ALTER TABLE Lid DROP COLUMN VrijwillgersVasteTaak;
-ALTER TABLE Lid DROP COLUMN VrijwillgersAfgekocht;
-ALTER TABLE Lid DROP COLUMN VrijwillgersToelichting;
-ALTER TABLE Lid DROP COLUMN Extra1;
-ALTER TABLE Lid DROP COLUMN Extra2;
-ALTER TABLE Lid DROP COLUMN Extra3;
-ALTER TABLE Lid DROP COLUMN Extra4;
-ALTER TABLE Lid DROP COLUMN Extra5;
-ALTER TABLE Lid DROP COLUMN ExtraB;
-ALTER TABLE Lid DROP COLUMN ExtraC;
-ALTER TABLE Lid DROP COLUMN ExtraD;
-ALTER TABLE Lid DROP COLUMN ExtraE;
-ALTER TABLE Lid DROP COLUMN ToegangsCode;
-ALTER TABLE Lid DROP COLUMN Image;
-ALTER TABLE Lid DROP COLUMN VrijwilligersKorting;
-ALTER TABLE Lid DROP COLUMN Ouder2_Naam;
-ALTER TABLE Lid DROP COLUMN Ouder2_Email1;
-ALTER TABLE Lid DROP COLUMN Ouder2_Email2;
-ALTER TABLE Lid DROP COLUMN Ouder2_Telefoon;
-ALTER TABLE Lid CHANGE Ouder2_Mobiel Ouder1_Mobiel2 char(20);
-ALTER TABLE Lid CHANGE ExtraA TrainingsGroepen varchar(50);
-UPDATE Lid SET TrainingsGroepen='' WHERE TrainingsGroepen = '_ExtraA';
+app toevoegen aan manifest.webmanifest en ngsw-config.json
+
 
 
 */
